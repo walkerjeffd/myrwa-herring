041c8c085173cc61f84356f8f271fc63742e19a9
Jeff Walker <walker.jeff.d@gmail.com>
2017-03-27 18:18:18 -0400

    add permissions script for db

---
+++ db/permissions.sql
@@ -0,0 +1,8 @@
+-- create new user manually in psql
+-- CREATE ROLE myrwa_www WITH LOGIN PASSWORD ''
+
+GRANT CONNECT ON DATABASE herring TO myrwa_www;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO myrwa_www;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO myrwa_www;
+GRANT SELECT,INSERT ON TABLE count TO myrwa_www;
