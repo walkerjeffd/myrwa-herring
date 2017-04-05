f0879694c7d1411efe10ace7ed4cd5c31b37d0bb
Jeff Walker <walker.jeff.d@gmail.com>
2017-04-05 17:23:32 -0400

    replace vue/d3 with highcharts in video-status

--- video-status/src/main.js
+++ video-status/src/main.js
@@ -1,15 +1,150 @@
-import Vue from 'vue'
-import VueResource from 'vue-resource'
+var $ = require('jquery'),
+    Highcharts = require('highcharts'),
+    d3 = require('d3');
 
-Vue.use(VueResource)
+require('highcharts/modules/exporting')(Highcharts);
 
-import App from './App.vue'
-import DonutChart from './DonutChart.vue'
+var data = d3.timeDay
+  .range(new Date(2016, 3, 4), new Date(2016, 3, 30), 1)
+  .map(function (d) {
+    var x = {
+      date: d,
+      n_count: Math.floor(Math.random() * 10),
+      n_fish: Math.floor(Math.random() * Math.random() * 100 * 10)
+    };
 
-Vue.component('donut-chart', DonutChart)
+    x.mean_fish = x.n_count > 0 ? x.n_fish / x.n_count : 0;
 
-window.onload = function () {
-  new Vue({
-    render: h => h(App)
-  }).$mount('#app')
-}
+    return x;
+  });
+
+Highcharts.setOptions({
+  global: {
+    useUTC: false
+  }
+});
+
+var colors = Highcharts.getOptions().colors;
+
+Highcharts.chart('mrh-counts', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Number of Video Counts per Day'
+  },
+  xAxis: {
+    type: 'datetime',
+    dateTimeLabelFormats: {
+      month: '%b %d',
+      day: '%b %d'
+    }
+  },
+  yAxis: {
+    title: {
+      text: '# Videos Counted'
+    }
+  },
+  legend: {
+    enabled: false
+  },
+  series: [{
+    name: 'Counts per day',
+    data: data.map(function (d) {
+      return [d.date.valueOf(), d.n_count]
+    }),
+    color: colors[2]
+  }]
+});
+
+Highcharts.chart('mrh-fish', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Number of Fish Counted per Day'
+  },
+  xAxis: {
+    type: 'datetime',
+    dateTimeLabelFormats: {
+      month: '%b %d',
+      day: '%b %d'
+    }
+  },
+  yAxis: {
+    title: {
+      text: '# Fish Counted'
+    }
+  },
+  legend: {
+    enabled: false
+  },
+  series: [{
+    name: 'Fish per day',
+    data: data.map(function (d) {
+      return [d.date.valueOf(), d.n_fish]
+    }),
+    color: colors[4]
+  }]
+});
+
+Highcharts.chart('mrh-fish-avg', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Average Number of Fish Counted per Day'
+  },
+  xAxis: {
+    type: 'datetime',
+    dateTimeLabelFormats: {
+      month: '%b %d',
+      day: '%b %d'
+    }
+  },
+  yAxis: {
+    title: {
+      text: '# Fish Counted'
+    }
+  },
+  legend: {
+    enabled: false
+  },
+  series: [{
+    name: 'Fish per day',
+    data: data.map(function (d) {
+      return [d.date.valueOf(), d.mean_fish]
+    }),
+    color: colors[3]
+  }]
+});
+
+Highcharts.chart('mrh-pct-watched', {
+  chart: {
+    type: 'pie'
+  },
+  title: {
+    text: 'Videos Watched'
+  },
+  plotOptions: {
+    pie: {
+      shadow: false,
+      center: ['50%', '50%']
+    }
+  },
+  series: [{
+    name: '# Videos',
+    size: '60%',
+    innerSize: '50%',
+    data: [{
+      name: 'Watched',
+      y: Math.floor(Math.random() * 100),
+      color: colors[0]
+    }, {
+      name: 'Not Watched',
+      y: Math.floor(Math.random() * 100),
+      color: colors[1]
+    }]
+  }]
+});
+
