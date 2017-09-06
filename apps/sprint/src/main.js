3e4903de25203988defbc53f7b0afd706c8f8432
Jeff Walker <walker.jeff.d@gmail.com>
2017-09-06 09:42:27 -0400

    move sprint app below banner

--- apps/sprint/src/main.js
+++ apps/sprint/src/main.js
@@ -6,12 +6,10 @@ var config = require('../../config');
 require('./css/app.css');
 
 window.onload = function () {
-  $('#siteWrapper > div.banner-thumbnail-wrapper.has-description > div.desc-wrapper > p:nth-child(3)').after(
-    `<div class="sprint-container">
-      <p class="sprint-text">Help us meet our goal of watching 1,000 videos by the end of September!</p>
-      <div id="sprint-progressbar-circle" class="sprint-text"></div>
-      <p class="sprint-remaining sprint-text"></p>
-    </div>`
+  $('.sprint-container').append(
+    `<p class="sprint-text">Help us meet our goal of watching 1,000 videos by the end of September!</p>
+    <div id="sprint-progressbar-circle" class="sprint-text"></div>
+    <p class="sprint-remaining sprint-text"></p>`
   );
   $.get(config.api.url + '/sprint/', function (response) {
     var count = +response.data.count;
