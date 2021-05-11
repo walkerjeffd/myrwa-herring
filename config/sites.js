303415628d2c8316a0441c4ae643c9307322d658
Jeff Walker <jeff@walkerenvres.com>
2021-05-11 13:25:57 +0000

    update uml

--- config/sites.js
+++ config/sites.js
@@ -32,30 +32,31 @@ module.exports = {
   },
   UML: {
     window: {
-      start_date: '2020-04-24',
-      end_date: '2020-07-01',
+      start_date: '2021-04-29',
+      end_date: '2021-07-01',
       method: 'fixed'
     },
     times: {
-      start_hour: 5,
-      end_hour: 20
+      start_hour: 7,
+      end_hour: 18
     },
     counts: {
       min_count_n: 0,
-      max_count_n: 10,
+      max_count_n: 2,
       min_count_mean: 0
     },
-    //sampler: {
+    sampler: {
       //distribution: 'exponential',
       //lambda: 0.0005
-    //},
+      distribution: 'uniform'
+    },
     status: {
-      start_date: '2020-04-24',
-      end_date: '2020-07-01'
+      start_date: '2021-04-29',
+      end_date: '2021-07-01'
     },
     run: {
-      start_date: '2020-04-24',
-      end_date: '2020-07-01'
+      start_date: '2021-04-29',
+      end_date: '2021-07-01'
     }
   },
   PLY: {
